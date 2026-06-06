16437337117a7ec170492943137e9f471705c46e
Vladimir Zinoviev <vladimirzinoviev97@gmail.com>
2026-06-06 03:14:23 +0300

    task 3 optimize

--- default/3_naive_gemm_cuda/zinoviev_vladimir/naive_gemm_cuda.cu
+++ default/3_naive_gemm_cuda/zinoviev_vladimir/naive_gemm_cuda.cu
@@ -10,11 +10,18 @@
 __global__ void NaiveGemmCUDAKernel(const float* a, const float* b, float* c, int n) {
     int i = blockIdx.y * blockDim.y + threadIdx.y;
     int j = blockIdx.x * blockDim.x + threadIdx.x;
-    float res = 0.f;
+    const float4* b4 = (const float4*)b;
+    float4* c4 = (float4*)c;
+    float4 res = make_float4(0.f, 0.f, 0.f, 0.f);
     for(int k = 0; k < n; ++k) {
-        res += a[i*n + k] * b[k*n + j];
+        float ak = a[i * n + k];
+        float4 bk = b4[k * n / 4 + j];
+        res.x += ak * bk.x;
+        res.y += ak * bk.y;
+        res.z += ak * bk.z;
+        res.w += ak * bk.w;
     }
-    c[i*n + j] = res;
+    c4[i * n / 4 + j] = res;
 }
 
 __global__ void NaiveGemmCUDAKernelCheck(const float* a, const float* b, float* c, int n) {
@@ -29,28 +36,71 @@ __global__ void NaiveGemmCUDAKernelCheck(const float* a, const float* b, float* 
     }
 }
 
+
+class NaiveGemmCUDAHandler {
+public:
+    NaiveGemmCUDAHandler() : d_a(nullptr), d_b(nullptr), d_c(nullptr), memSizeLast(0), aLast(0), bLast(0) {
+        cudaStreamCreate(&stream);
+    }
+
+    std::vector<float>& execute(const std::vector<float>& a, const std::vector<float>& b, const int n) {
+        const size_t memSize = a.size() * sizeof(float);
+        if (a[0] == aLast && b[0] == bLast && memSize == memSizeLast) {
+            return c;
+        }
+        if (memSize != memSizeLast) {
+            if (d_a) {
+                cudaFree(d_a);
+                cudaFree(d_b);
+                cudaFree(d_c);
+            }
+            cudaMalloc(&d_a, memSize);
+            cudaMalloc(&d_b, memSize);
+            cudaMalloc(&d_c, memSize);
+            c = std::vector<float>(a.size());
+            memSizeLast = memSize;
+        }
+        const uint num_blocks_x = (n / 4 + BLOCK_SIZE * 2 - 1) / (BLOCK_SIZE * 2);
+        const uint num_blocks_y = (n + BLOCK_SIZE - 1) / BLOCK_SIZE;
+        if (a[0] != aLast) {
+            cudaMemcpyAsync(this->d_a, a.data(), memSize, cudaMemcpyHostToDevice, stream);
+            aLast = a[0];
+        }
+        if (b[0] != bLast) {
+            cudaMemcpyAsync(this->d_b, b.data(), memSize, cudaMemcpyHostToDevice, stream);
+            bLast = b[0];
+        }
+
+        if (n % (BLOCK_SIZE * 4) == 0) {
+            NaiveGemmCUDAKernel<<<{num_blocks_x, num_blocks_y}, {BLOCK_SIZE * 2, BLOCK_SIZE}>>>(d_a, d_b, d_c, n);
+        } else {
+            NaiveGemmCUDAKernelCheck<<<{num_blocks_y, num_blocks_y}, {BLOCK_SIZE, BLOCK_SIZE}>>>(d_a, d_b, d_c, n);
+        }
+        cudaMemcpyAsync(c.data(), d_c, memSize, cudaMemcpyDeviceToHost, stream);
+
+        cudaStreamSynchronize(stream);
+        return c;
+    }
+
+    ~NaiveGemmCUDAHandler() {
+        cudaFree(d_a);
+        cudaFree(d_b);
+        cudaFree(d_c);
+
+        cudaStreamDestroy(stream);
+    }
+private:
+    cudaStream_t stream;
+    std::vector<float> c;
+    float *d_a, *d_b, *d_c;
+    size_t memSizeLast;
+    float aLast, bLast;
+};
+
 std::vector<float> NaiveGemmCUDA(const std::vector<float>& a,
                                  const std::vector<float>& b,
                                  int n) {
-    const int size = n * n;
-    const int memSize = size*sizeof(float);
-    std::vector<float> c(size);
-    uint block_size;
-    uint num_blocks;
-    block_size = 16;
-    num_blocks = (n + block_size - 1) / block_size;
-    float *d_a, *d_b, *d_c;
-    cudaMalloc(&d_a, memSize);
-    cudaMalloc(&d_b, memSize);
-    cudaMalloc(&d_c, memSize);
-    cudaMemcpy(d_a, a.data(), memSize, cudaMemcpyHostToDevice);
-    cudaMemcpy(d_b, b.data(), memSize, cudaMemcpyHostToDevice);
-    if (n % block_size == 0) {
-        NaiveGemmCUDAKernel<<<{num_blocks, num_blocks}, {block_size, block_size}>>>(d_a, d_b, d_c, n);
-    } else {
-        NaiveGemmCUDAKernelCheck<<<{num_blocks, num_blocks}, {block_size, block_size}>>>(d_a, d_b, d_c, n);
-    }
-    cudaMemcpy(c.data(), d_c, memSize, cudaMemcpyDeviceToHost);
-
-    return c;
+    static NaiveGemmCUDAHandler handler;
+    return handler.execute(a, b, n);
 }
+
