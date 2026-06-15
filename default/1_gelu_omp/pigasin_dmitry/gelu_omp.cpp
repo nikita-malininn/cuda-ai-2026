83b06a111fef69b850679c42e947017c5294036d
Dmitry Pigasin <d.pigasin@yadro.com>
2026-06-15 18:39:41 +0300

    Update 1_gelu_omp

--- default/1_gelu_omp/pigasin_dmitry/gelu_omp.cpp
+++ default/1_gelu_omp/pigasin_dmitry/gelu_omp.cpp
@@ -4,35 +4,34 @@
 
 #include <cmath>
 
-std::vector<float> GeluRef(const std::vector<float> &input) {
-    constexpr float sqrt_2_over_pi = 0.7978845608f;
-    constexpr float coef = 0.044715f;
 
+inline float gelu(float x) {
+    return 0.5f * x * (1.0f + std::tanh(0.79788456080286541f * (x + 0.044715f * x * x * x)));
+}
+
+inline float fastTanh(float x) {
+    return 1.f - (2.f / (1.f + std::exp(x * 2.f)));
+}
+
+inline float fastGelu(float x) {
+    return 0.5f * x * (1.0f + fastTanh(0.79788456080286541f * (x + 0.044715f * x * x * x)));
+}
+
+std::vector<float> GeluRef(const std::vector<float> &input) {
     const size_t size = input.size();
     std::vector<float> output(size);
 
     const float *__restrict in_ptr = input.data();
     float *__restrict out_ptr = output.data();
 
-    #pragma omp parallel for simd
     for (size_t i = 0; i < size; ++i) {
-        const float x = in_ptr[i];
-        const float x_cube = x * x * x;
-        const float inner = sqrt_2_over_pi * (x + coef * x_cube);
-        out_ptr[i] = 0.5f * x * (1.0f + std::tanh(inner));
+        out_ptr[i] = gelu(in_ptr[i]);
     }
 
     return output;
 }
 
-inline float tanh(float x) {
-    return 1.f - (2.f / (1.f + std::exp(x * 2.f)));
-}
-
 std::vector<float> GeluOMP(const std::vector<float> &input) {
-    constexpr float sqrt_2_over_pi = 0.7978845608f;
-    constexpr float coef = 0.044715f;
-
     const size_t size = input.size();
     std::vector<float> output(size);
 
@@ -41,10 +40,7 @@ std::vector<float> GeluOMP(const std::vector<float> &input) {
 
     #pragma omp parallel for simd
     for (size_t i = 0; i < size; ++i) {
-        const float x = in_ptr[i];
-        const float x_cube = x * x * x;
-        const float inner = sqrt_2_over_pi * (x + coef * x_cube);
-        out_ptr[i] = 0.5f * x * (1.0f + tanh(inner));
+        out_ptr[i] = fastGelu(in_ptr[i]);
     }
 
     return output;
