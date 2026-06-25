9535026a1f280cd101af73b7e621cb9f1a141c0f
Nikolay <n.salnikov@yadro.com>
2026-06-25 20:56:43 +0000

    Submit 5_gemm_cublas

---
+++ default/5_gemm_cublas/salnikov_nikolay/gemm_cublas.cu
@@ -0,0 +1,46 @@
+#include "gemm_cublas.h"
+#include <cublas_v2.h>
+
+std::vector<float> GemmCUBLAS(const std::vector<float>& a,
+                              const std::vector<float>& b,
+                              int n)
+{
+    cublasHandle_t cublas;
+    cublasCreate(&cublas);
+
+    float* A = nullptr;
+    float* B = nullptr;
+    float* C = nullptr;
+
+    int size = n * n * sizeof(float);
+    cudaMalloc(&A, size);
+    cudaMalloc(&B, size);
+    cudaMalloc(&C, size);
+
+    cudaMemcpy(A, a.data(), size, cudaMemcpyHostToDevice);
+    cudaMemcpy(B, b.data(), size, cudaMemcpyHostToDevice);
+
+    float alpha = 1.f;
+    float beta = 0.f;
+    cublasSgemm(cublas,
+                CUBLAS_OP_N, CUBLAS_OP_N,
+                n, n, n,
+                &alpha, B, n, A, n,
+                &beta, C, n);
+
+    std::vector<float> c(n * n);
+    cudaMemcpy(c.data(), C, size, cudaMemcpyDeviceToHost);
+
+    cudaFree(A);
+    cudaFree(B);
+    cudaFree(C);
+
+    cublasDestroy(cublas);
+
+    return c;
+}
+
+int main()
+{
+    return 0;
+}
